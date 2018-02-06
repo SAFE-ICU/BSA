63583ce80b3941aecaca1127a5b42d10ae7e9e5e
Tarun N <tnarasim@stanford.edu>
2018-02-05 16:01:21 -0800

    added pull_data folder

---
+++ pull_data/pull_data.sql
@@ -0,0 +1,30 @@
+# Everyone with inpatient admission
+
+
+# Everyone with inpatient admission 
+#   - their medications while inpatient
+
+
+# Everyone with inpatient admission 
+#   - medications
+#   - demographic data
+
+
+# Everyone with inpatient admission 
+#   - medications
+#   - demographic data
+#   - diagnoses (within time window after admissions?)
+
+
+# Everyone with inpatient admission 
+#   - medications
+#   - demographic data
+#   - diagnoses (within time window after admissions?)
+#   - insurance plan
+
+# Everyone with inpatient admission 
+#   - medications
+#   - demographic data
+#   - diagnoses (within time window after admissions?)
+#   - insurance plan
+#   - department ID
